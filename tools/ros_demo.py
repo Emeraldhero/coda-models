de1f5f8827da8464c3709643edf048620fb22623
Omkarthikeya G <101446434+Emeraldhero@users.noreply.github.com>
2023-12-07 01:34:57 +0530

    Update ros_demo.py

--- tools/ros_demo.py
+++ tools/ros_demo.py
@@ -60,41 +60,84 @@ def point_cloud_callback(msg):
 
     pc_msg_queue.put(msg)
 
+# def main():
+#     args, cfg = parse_config()
+#     logger = common_utils.create_logger()
+#     logger.info('-----------------ROS Demo of OpenPCDet-------------------------')
+
+#     #1 Fill in dummy dataset to set point features values
+#     dummy_dataset = DemoDataset(
+#         dataset_cfg=cfg.DATA_CONFIG, class_names=cfg.CLASS_NAMES, training=False,
+#         root_path=Path("../data"), ext=".bin", logger=logger
+#     )
+#     color_map=normalize_color(coda_utils.BBOX_ID_TO_COLOR)
+
+#     #2 Load model
+#     model = build_network(model_cfg=cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=dummy_dataset)
+#     model.load_params_from_file(filename=args.ckpt, logger=logger, to_cpu=True)
+#     model.cuda()
+#     model.eval()
+
+#     #3 Initialize ROS
+#     pc_topic = args.pc
+#     rospy.init_node('CODaROSDetector', anonymous=True)
+#     rospy.Subscriber(pc_topic, PointCloud2, point_cloud_callback)
+#     bbox_3d_pub = rospy.Publisher('/coda/bbox_3d', MarkerArray, queue_size=10)
+
+#     #4 Load dummy data to speed up first pass
+#     dummy_pc = np.random.rand(1000, 3).astype(np.float32)
+#     dummy_data_dict = V.pcnp_to_datadict(dummy_pc, dummy_dataset, frame_id=0)
+#     pred_dicts, _ = model.forward(dummy_data_dict)
+    
+#     logger.info("Model initalized...")
+#     while not rospy.is_shutdown():
+#         if not pc_msg_queue.empty():
+#             pc_msg = pc_msg_queue.get()
+            
+#             V.visualize_3d(model, dummy_dataset, pc_msg, bbox_3d_pub, color_map, logger)
+#     logger.info("Demo complete, cleaning up...")
+
+# if __name__ == '__main__':
+#     main()
 def main():
     args, cfg = parse_config()
     logger = common_utils.create_logger()
     logger.info('-----------------ROS Demo of OpenPCDet-------------------------')
 
-    #1 Fill in dummy dataset to set point features values
+    # 1. Fill in dummy dataset to set point features values
     dummy_dataset = DemoDataset(
         dataset_cfg=cfg.DATA_CONFIG, class_names=cfg.CLASS_NAMES, training=False,
         root_path=Path("../data"), ext=".bin", logger=logger
     )
-    color_map=normalize_color(coda_utils.BBOX_ID_TO_COLOR)
+    color_map = normalize_color(coda_utils.BBOX_ID_TO_COLOR)
 
-    #2 Load model
+    # 2. Load model
     model = build_network(model_cfg=cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=dummy_dataset)
     model.load_params_from_file(filename=args.ckpt, logger=logger, to_cpu=True)
+
+    # Move the model to the GPU after loading the parameters
     model.cuda()
     model.eval()
 
-    #3 Initialize ROS
+    # 3. Initialize ROS
     pc_topic = args.pc
     rospy.init_node('CODaROSDetector', anonymous=True)
     rospy.Subscriber(pc_topic, PointCloud2, point_cloud_callback)
     bbox_3d_pub = rospy.Publisher('/coda/bbox_3d', MarkerArray, queue_size=10)
 
-    #4 Load dummy data to speed up first pass
+    # 4. Load dummy data to speed up the first pass
     dummy_pc = np.random.rand(1000, 3).astype(np.float32)
     dummy_data_dict = V.pcnp_to_datadict(dummy_pc, dummy_dataset, frame_id=0)
-    pred_dicts, _ = model.forward(dummy_data_dict)
-    
-    logger.info("Model initalized...")
+    with torch.no_grad():  # Avoid unnecessary gradient computation
+        pred_dicts, _ = model.forward(dummy_data_dict)
+
+    logger.info("Model initialized...")
     while not rospy.is_shutdown():
         if not pc_msg_queue.empty():
             pc_msg = pc_msg_queue.get()
-            
-            V.visualize_3d(model, dummy_dataset, pc_msg, bbox_3d_pub, color_map, logger)
+            with torch.no_grad():  # Avoid unnecessary gradient computation
+                V.visualize_3d(model, dummy_dataset, pc_msg, bbox_3d_pub, color_map, logger)
+
     logger.info("Demo complete, cleaning up...")
 
 if __name__ == '__main__':
